6dd2a59da7124929de1ca85c1f4991cfbf151d5f
Ryo Hirai <m1zuk1lac1e@gmail.com>
2023-07-12 02:41:44 +0900

    Add: 自機の実装

--- game.py
+++ game.py
@@ -1,16 +1,26 @@
 import pygame
 from setting import *
+from player import Player
 
 class Game():
 
     def __init__(self):
         self.screen = pygame.display.get_surface()
 
+        # グループの作成
+        self.create_group()
+
+        # プレイヤーの作成
+        player = Player(self.player_group, 300, 500)
+
         # 背景
         self.pre_bg_img = pygame.image.load('assets/img/background/bg.png')
         self.bg_img = pygame.transform.scale(self.pre_bg_img, (screen_width, screen_height))
         self.bg_y = 0
         self.scroll_speed = 0.5
+
+    def create_group(self):
+        self.player_group = pygame.sprite.GroupSingle()
 
     def scroll_bg(self):
         self.bg_y = (self.bg_y + self.scroll_speed) % screen_height
@@ -19,3 +29,7 @@ class Game():
 
     def run(self):
         self.scroll_bg()
+
+        # グループの描画と更新
+        self.player_group.draw(self.screen)
+        self.player_group.update()

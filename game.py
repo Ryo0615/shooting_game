6f3cd0016890f4c9760cf3ebf71b1e49283e86bd
Ryo Hirai <m1zuk1lac1e@gmail.com>
2023-07-12 02:09:13 +0900

    Add: 背景画面の実装

---
+++ game.py
@@ -0,0 +1,21 @@
+import pygame
+from setting import *
+
+class Game():
+
+    def __init__(self):
+        self.screen = pygame.display.get_surface()
+
+        # 背景
+        self.pre_bg_img = pygame.image.load('assets/img/background/bg.png')
+        self.bg_img = pygame.transform.scale(self.pre_bg_img, (screen_width, screen_height))
+        self.bg_y = 0
+        self.scroll_speed = 0.5
+
+    def scroll_bg(self):
+        self.bg_y = (self.bg_y + self.scroll_speed) % screen_height
+        self.screen.blit(self.bg_img, (0, self.bg_y - screen_height)) # y: -600 ~ -1
+        self.screen.blit(self.bg_img, (0, self.bg_y)) # y: 0 ~ 599
+
+    def run(self):
+        self.scroll_bg()
---
+++ main.py
@@ -0,0 +1,41 @@
+import pygame
+from setting import *
+from game import Game
+
+pygame.init()
+
+# ウィンドウの作成
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Shooting Game")
+
+# FPSの設定
+clock = pygame.time.Clock()
+
+# ゲームオブジェクトの作成
+game = Game()
+
+# メインループ ======================================================================================================================
+run = True
+while run:
+
+    # 背景の塗りつぶし
+    screen.fill(BLACK)
+
+    # ゲームの実行
+    game.run()
+
+    # イベント取得
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                run = False
+
+    # 画面の更新
+    pygame.display.update()
+    clock.tick(FPS)
+
+# ==================================================================================================================================
+
+pygame.quit()

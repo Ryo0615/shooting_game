4cad1832fc40789c7dfaadbc68298c73e291f83f
Ryo Hirai <m1zuk1lac1e@gmail.com>
2023-07-13 01:21:09 +0900

    Add: 敵の実装

--- game.py
+++ game.py
@@ -1,6 +1,8 @@
 import pygame
 from setting import *
 from player import Player
+from enemy import Enemy
+import random
 
 class Game():
 
@@ -13,6 +15,9 @@ class Game():
         # プレイヤーの作成
         player = Player(self.player_group, 300, 500)
 
+        # 敵の作成
+        self.timer = 0
+
         # 背景
         self.pre_bg_img = pygame.image.load('assets/img/background/bg.png')
         self.bg_img = pygame.transform.scale(self.pre_bg_img, (screen_width, screen_height))
@@ -21,6 +26,13 @@ class Game():
 
     def create_group(self):
         self.player_group = pygame.sprite.GroupSingle()
+        self.enemy_group = pygame.sprite.Group()
+
+    def create_enemy(self):
+        self.timer += 1
+        if self.timer > 50:
+            enemy = Enemy(self.enemy_group, random.randint(50, 550), 0)
+            self.timer = 0
 
     def scroll_bg(self):
         self.bg_y = (self.bg_y + self.scroll_speed) % screen_height
@@ -30,6 +42,10 @@ class Game():
     def run(self):
         self.scroll_bg()
 
+        self.create_enemy()
+
         # グループの描画と更新
         self.player_group.draw(self.screen)
         self.player_group.update()
+        self.enemy_group.draw(self.screen)
+        self.enemy_group.update()
